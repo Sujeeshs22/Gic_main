6162b525b0ce726e0718e74f733ad184895f9f8d
sujeesh s <>
2025-09-27 12:23:11 +0530

    code issues fixed

--- gic/common/api_serializers.py
+++ gic/common/api_serializers.py
@@ -1,26 +1,29 @@
 from rest_framework import serializers
-from .models import User
+from django.contrib.auth import get_user_model
+from .models import Profile
+
+User = get_user_model()
 
 
 class FileUploadSerializer(serializers.Serializer):
     file = serializers.FileField()
 
 
-class UserSerializer(serializers.ModelSerializer):
-    name = serializers.CharField(max_length=150)
-    email = serializers.EmailField()
-    age = serializers.IntegerField()
+class UserProfileSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(source="user.username", max_length=150)
+    email = serializers.EmailField(source="user.email")
+    age = serializers.IntegerField(required=False)
 
     class Meta:
-        model = User
+        model = Profile
         fields = ["name", "email", "age"]
 
     def validate_name(self, value):
         if not value.strip():
-            raise serializers.ValidationError("name is given empty")
+            raise serializers.ValidationError("Name cannot be empty")
         return value
 
     def validate_age(self, value):
-        if not (0 <= value <= 120):
-            raise serializers.ValidationError("age must be between 0 and 120")
+        if value is not None and not (0 <= value <= 120):
+            raise serializers.ValidationError("Age must be between 0 and 120")
         return value
